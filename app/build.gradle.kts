d35d45a985873d1f8c4b5f1961e730e3f1ef2f2e
Hathibelagal <42626106+hathibelagal-dev@users.noreply.github.com>
2025-02-04 03:38:46 +0530

    whitespace removal

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "io.github.hathibelagal.eidetic"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.2.1"
+        versionCode = 5
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
